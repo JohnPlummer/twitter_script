deddc882a0540ce5ff3783ffd58c3ea0c2d27e8d
John Plummer <john@memphis.co.uk>
2011-05-28 00:58:35 +0100

    Project skeleton.

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+gem 'sqlite3'
+gem 'haml'
+gem 'sass'
+gem 'compass'
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'ruby-debug19'
+  gem 'ruby-debug-ide'
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+  gem 'shoulda'
+  gem 'capybara'
+  gem 'launchy'
+  gem 'spork'
+  gem 'factory_girl_rails'
+  gem 'annotate-models'
+  gem 'haml-rails'
+  gem 'rake', '~> 0.8.7' #Until the Rake 0.9 Rails Bug is fixed
+  gem 'heroku'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterScript::Application.config.secret_token = '3b6f30d9e56356010c0d43dfbca2ce3ad553ffc452679fcce47274b8d54d738c218bf6e8763128635f5abde700830ed7a75e814c1cfee9e749d5065003df9868'
